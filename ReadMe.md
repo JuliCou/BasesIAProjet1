4d5f3e0da06dc7c657efd2fa213cf2d5b65f1f10
Julie Courgibet <julie.courgibet@cgi.com>
2019-12-10 15:12:16 +0100

    First Commit

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+## Projet Base de l'IA
+Collaborators:
+Balti Youssef
+Julie Courgibet
+
+# Part 1: k-nearest neighbors algorithm
---
+++ project.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 27 09:48:53 2019
+
+@author: Julie Courgibet
+Balti Youssef
+"""
